643f03ff9500c85414095522509d1e827dffa742
doingmov <yohei13111@mail.ru>
2026-09-21 16:26:14 +0300

    echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,23 +1,28 @@
 import os
+import random
 
 import vk_api
 from dotenv import load_dotenv
 from vk_api.longpoll import VkEventType, VkLongPoll
 
 
+def echo(event: vk_api.longpoll.Event, vk_client) -> None:
+    vk_client.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.getrandbits(31),
+    )
+
+
 def main() -> None:
     load_dotenv()
     vk_session = vk_api.VkApi(token=os.environ['VK_GROUP_TOKEN'])
+    vk_client = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от:', event.user_id)
-            else:
-                print('От меня для:', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_client)
 
 
 if __name__ == '__main__':
